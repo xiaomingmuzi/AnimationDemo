c79ed68324553c0a4ae4ff9bef2cb88187b921c5
xiaomingmuzi <946384750@qq.com>
2017-04-14 17:42:50 +0800

    添加加速器动画效果

---
+++ app/src/main/java/com/lixm/animationdemo/animator/PathEvaluator.java
@@ -0,0 +1,23 @@
+package com.lixm.animationdemo.animator;
+
+import android.animation.TypeEvaluator;
+
+import com.lixm.animationdemo.bean.Point;
+
+/**
+ * @author Lixm
+ * @date 2017/4/14
+ * @detail
+ */
+
+public class PathEvaluator implements TypeEvaluator {
+    @Override
+    public Object evaluate(float fraction, Object startValue, Object endValue) {
+        Point startPoint= (Point) startValue;
+        Point endPoint= (Point) endValue;
+        float x=startPoint.getX()+fraction*(endPoint.getX()-startPoint.getX()) ;
+        float y=startPoint.getY()+fraction*(endPoint.getY()-startPoint.getY());
+        Point point=new Point(x,y);
+        return point;
+    }
+}
---
+++ app/src/main/java/com/lixm/animationdemo/customview/MyAnimPathView.java
@@ -0,0 +1,19 @@
+package com.lixm.animationdemo.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author Lixm
+ * @date 2017/4/14
+ * @detail
+ */
+
+public class MyAnimPathView extends View {
+
+    public MyAnimPathView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
