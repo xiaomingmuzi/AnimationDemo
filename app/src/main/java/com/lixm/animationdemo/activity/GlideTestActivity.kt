27bab7ee4b43738afa9511302c9048818d656d33
xiaomingmuzi <946384750@qq.com>
2018-05-17 09:36:11 +0800

    修改本地存储

--- app/src/main/java/com/lixm/animationdemo/activity/GlideTestActivity.kt
+++ app/src/main/java/com/lixm/animationdemo/activity/GlideTestActivity.kt
@@ -4,17 +4,28 @@ import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.lixm.animationdemo.R
+import com.lixm.animationdemo.adapter.GlideRecycleViewAdapter
+import com.lixm.animationdemo.bean.GlideBean
 
 class GlideTestActivity : BaseActivity() {
 
     private val context = this
-    private val rv_1 by lazy{
+    private var mDatas = ArrayList<GlideBean>()
+    private val adapter: GlideRecycleViewAdapter = GlideRecycleViewAdapter(context, mDatas)
+    private val rv_1 by lazy {
         findViewById<RecyclerView>(R.id.rv_1)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_glide_test)
-        val manager:GridLayoutManager
+        val manager = GridLayoutManager(context, 2)
+        rv_1.layoutManager = manager
+        for (x in 0..10) {
+            println(x)
+            var bean = GlideBean(x, "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1526446784607&di=18a31bd64f00dc811bbb757de7bdb411&imgtype=0&src=http%3A%2F%2Fscimg.jb51.net%2Fallimg%2F150717%2F14-150GF9355MO.jpg")
+            mDatas.add(bean)
+        }
+        rv_1.adapter = adapter
     }
 }
--- app/src/main/java/com/lixm/animationdemo/adapter/GlideRecycleViewAdapter.java
+++ app/src/main/java/com/lixm/animationdemo/adapter/GlideRecycleViewAdapter.java
@@ -25,19 +25,48 @@ public class GlideRecycleViewAdapter extends RecyclerView.Adapter<GlideRecycleVi
     private Context context;
     private ArrayList<GlideBean> mDatas;
 
+    public GlideRecycleViewAdapter(Context context, ArrayList<GlideBean> mDatas) {
+        this.context = context;
+        this.mDatas = mDatas;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.glide_item,parent,false));
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.glide_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        GlideBean bean=mDatas.get(position);
-        switch (bean.getType()){
+        GlideBean bean = mDatas.get(position);
+        switch (bean.getType()) {
             case 0:
-                Glide.with(context).load(bean.getUrl()).error(R.mipmap.failed_pic).placeholder(R.mipmap.place_pic).into(holder.img);
+                // 强制静态图
+                Glide.with(context)
+                        .load(bean.getUrl())
+                        .asBitmap()
+                        .placeholder(R.mipmap.place_pic)
+                        .error(R.mipmap.failed_pic)
+                        .into(holder.img);
+                holder.txt.setText("设置静态");
                 break;
-
+            case 1:
+                // 强制动态图(注意如果不是动态图，会报错，但是添加了error()方法之后可以捕获)
+                Glide.with(context)
+                        .load(bean.getUrl())
+                        .asGif()
+                        .placeholder(R.mipmap.place_pic)
+                        .error(R.mipmap.failed_pic)
+                        .into(holder.img);
+                holder.txt.setText("设置动态");
+                break;
+                default:
+                    Glide.with(context)
+                            .load(bean.getUrl())
+                            .error(R.mipmap.failed_pic)
+                            .placeholder(R.mipmap.place_pic)
+                            .into(holder.img);
+                    holder.txt.setText("普通显示");
+                    break;
         }
     }
 
@@ -46,15 +75,15 @@ public class GlideRecycleViewAdapter extends RecyclerView.Adapter<GlideRecycleVi
         return mDatas.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder{
+    class ViewHolder extends RecyclerView.ViewHolder {
 
         private ImageView img;
         private TextView txt;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            img=itemView.findViewById(R.id.glide_img);
-            txt=itemView.findViewById(R.id.glide_txt);
+            img = itemView.findViewById(R.id.glide_img);
+            txt = itemView.findViewById(R.id.glide_txt);
         }
     }
 }
